96b70a04200f213e8f03060ac236df00f177a1f0
Leila <leiila.hort@gmail.com>
2024-03-18 22:44:22 -0300

    tp3 completo

---
+++ Models/Computadores.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace GestaoDeLaboratorios.Models
+{
+    public class Computadores
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string  Marca { get; set;}
+
+        public string Processador { get; set;}
+        [DisplayName("Placa Mãe")]
+        public string PlacaMae { get; set; }
+
+        public int Memoria { get; set; }
+
+        public int HD { get; set; }
+        [DisplayName("Numero Patrimonio")]
+        public int NumeroPatrimonio { get; set; }
+        [DisplayName("Data De Compra")]
+        [DataType(DataType.Date)]
+        public string DataDeCompra { get; set; }
+
+
+    
+
+    }
+}
